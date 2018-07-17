5904cfd02ba3f26073515e46ad348bce8eb961ea
Zxheer_ <zaheer8668work@gmail.com>
2018-07-17 18:00:04 +0200

    Implemented HomeActivity
    Ran connection to API's using HttpHelper

--- app/src/main/java/com/zxdev/app/weatherapp2/HomeActivity.java
+++ app/src/main/java/com/zxdev/app/weatherapp2/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.zxdev.app.weatherapp2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.zxdev.app.weatherapp2.model.Weather;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(Double.toString(-46.00),Double.toString(48), "en");
+
     }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+
+            String[] data = new String[2];
+            Weather[] days = new com.zxdev.app.weatherapp2.model.Weather[0];
+            Forecast[] days2 = new Forecast[0];
+            days[0] = new Weather();
+            days2[1] = new Forecast();
+
+            Weather weather = new Weather();
+            data = ((new WeatherHttpClient()).getWeatherData(params[0], params[1], params[2]));
+
+            Log.e("TEST", data[0]+" "+data[1] );
+            return weather;
+        }
+
+    }
+
 }
