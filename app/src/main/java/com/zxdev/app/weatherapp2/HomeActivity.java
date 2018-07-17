7864b9597cb86438bfbee7e56d3a929664428818
Zxheer_ <zaheer8668work@gmail.com>
2018-07-17 20:47:00 +0200

    Added layout for HomeActivity

--- app/src/main/java/com/zxdev/app/weatherapp2/HomeActivity.java
+++ app/src/main/java/com/zxdev/app/weatherapp2/HomeActivity.java
@@ -7,9 +7,19 @@ import android.util.Log;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.constraint.ConstraintLayout;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtCurrentTemp, txtCurrentCondition, txtCity, txtCurrentMax, txtCurrentMin, txtCurrentT, txtTemp1, txtTemp2, txtTemp3, txtTemp4, txtTemp5, txtDay1, txtDay2, txtDay3, txtDay4, txtDay5;
+    private ImageView imgMain, imgDay1, imgDay2, imgDay3, imgDay4, imgDay5;
+    private Forecast days[] = new Forecast[5];
+    private ConstraintLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +79,44 @@ public class HomeActivity extends AppCompatActivity {
 
             return Days;
         }
+
+        @SuppressLint("SetTextI18n")
+        private void forecastGUI(Weather weather, String[] dayNames) {
+
+            ImageHelper.getImageByDescription(weather.getCondition(), imgMain);
+            ImageHelper.getImageByDescription(days[0].getCondition(), imgDay1);
+            ImageHelper.getImageByDescription(days[1].getCondition(), imgDay2);
+            ImageHelper.getImageByDescription(days[2].getCondition(), imgDay3);
+            ImageHelper.getImageByDescription(days[3].getCondition(), imgDay4);
+            ImageHelper.getImageByDescription(days[4].getCondition(), imgDay5);
+
+            if (weather.getCondition() == "Sunny")
+                main.setBackgroundColor(Color.parseColor("#47AB2F"));
+            else if (weather.getCondition() == "Cloudy")
+                main.setBackgroundColor(Color.parseColor("#54717A"));
+            else
+                main.setBackgroundColor(Color.parseColor("#57575D"));
+
+
+            txtCurrentTemp.setText((Math.round(weather.getTemp())) + "°");
+            txtCurrentT.setText((Math.round(weather.getTemp())) + "°");
+            txtCurrentMax.setText("  " + (Math.round(weather.getMaxTemp())) + "°");
+            txtCurrentMin.setText(" " + (Math.round(weather.getMinTemp())) + "°");
+            txtCurrentCondition.setText(weather.getCondition().toUpperCase());
+            txtCity.setText(weather.getCity());
+            txtTemp1.setText(" " + (Math.round(days[0].getTemp())) + "°");
+            txtTemp2.setText((Math.round(days[1].getTemp())) + "°");
+            txtTemp3.setText((Math.round(days[2].getTemp())) + "°");
+            txtTemp4.setText((Math.round(days[3].getTemp())) + "°");
+            txtTemp5.setText((Math.round(days[4].getTemp())) + "°");
+
+            txtDay1.setText(dayNames[0]);
+            txtDay2.setText(dayNames[1]);
+            txtDay3.setText(dayNames[2]);
+            txtDay4.setText(dayNames[3]);
+            txtDay5.setText(dayNames[4]);
+        }
+
 
     }
 
