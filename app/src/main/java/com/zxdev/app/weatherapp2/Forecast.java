5c6696681332ad708156da3fb6751686fadd0822
Zxheer_ <zaheer8668work@gmail.com>
2018-07-17 17:31:54 +0200

    Implemented Weather and Forecast classes

--- app/src/main/java/com/zxdev/app/weatherapp2/Forecast.java
+++ app/src/main/java/com/zxdev/app/weatherapp2/Forecast.java
@@ -1,4 +1,43 @@
 package com.zxdev.app.weatherapp2;
 
+import android.util.Log;
+
 public class Forecast {
+
+    private String condition;
+    private double temp;
+
+    public Forecast() {
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+    public void setCondition(String condition) {
+        switch (condition) {
+            case "Thunderstorm":
+            case "Drizzle":
+            case "Rain":
+            case "Snow":
+                condition = "Small-Rainy";
+                break;
+            case "Atmosphere":
+            case "Clouds":
+                condition = "Small-Cloudy";
+                break;
+            case "Clear":
+                condition = "Small-Sunny";
+                break;
+            default:
+                Log.e("Error", "Cant get weather conditon");
+                break;
+        }
+        this.condition = condition;
+    }
+    public double getTemp() {
+        return temp;
+    }
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
 }
--- app/src/main/java/com/zxdev/app/weatherapp2/Weather.java
+++ app/src/main/java/com/zxdev/app/weatherapp2/Weather.java
@@ -1,4 +1,67 @@
-package com.zxdev.app.weatherapp2;
+package com.zxdev.app.weatherapp2.model;
+
+import android.util.Log;
 
 public class Weather {
+
+    private String condition;
+    private String city;
+    private double temp;
+    private double minTemp;
+    private double maxTemp;
+
+    public Weather() {
+
+    };
+
+    public String getCondition() {
+        return condition;
+    }
+    public void setCondition(String condition) {
+
+        switch (condition) {
+            case "Thunderstorm":
+            case "Drizzle":
+            case "Rain":
+            case "Snow":
+                condition = "Rainy";
+                break;
+            case "Atmosphere":
+            case "Clouds":
+                condition = "Cloudy";
+                break;
+            case "Clear":
+                condition = "Sunny";
+                break;
+            default:
+                Log.e("Error", "Cant get weather conditon");
+                break;
+        }
+        this.condition = condition;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public double getTemp() {
+        return temp;
+    }
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+    public double getMinTemp() {
+        return minTemp;
+    }
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
 }
